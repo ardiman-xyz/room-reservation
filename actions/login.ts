513aee9a34e3696f5cda0738af9a54c98d357170
ardiman-xyz <pc.ardiman98@gmail.com>
2024-05-22 14:14:06 +0800

    ft: register user

--- actions/login.ts
+++ actions/login.ts
@@ -2,13 +2,28 @@
 
 import * as z from "zod";
 import {LoginSchema} from "@/schemas";
+import {db} from "@/lib/db";
+import bcrypt from "bcryptjs";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
-  if(!validatedFields) {
+  if(!validatedFields.success) {
     return {error: "invalid fields"};
   }
+
+  const { email, password } = validatedFields.data
+
+  const user = await db.user.findUnique({
+    where: {
+      email
+    }
+  });
+
+  if(!user) {
+    return {error: "user not found!"};
+  }
+
 
   return {success: "Login successfully"};
 
