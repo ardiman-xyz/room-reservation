c6260c7d5dd0f84f913f48ac3c9080ce050cdb40
ardiman-xyz <pc.ardiman98@gmail.com>
2024-05-26 21:30:35 +0800

    ft: dynamic sidebar route

--- app/(admin)/_components/desktop-sidebar.tsx
+++ app/(admin)/_components/desktop-sidebar.tsx
@@ -1,40 +1,50 @@
 import React from "react";
 import Link from "next/link";
-import {Bell, Home, Laptop, LineChart, Package, Package2, ShoppingCart, Users} from "lucide-react";
-
-import {Button} from "@/components/ui/button";
-import {Badge} from "@/components/ui/badge";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle
-} from "@/components/ui/card";
+    Bell,
+    Book,
+    Home,
+    Laptop,
+    LineChart,
+    Package,
+    Package2,
+    ShoppingCart,
+    Users,
+    Calendar,
+    Settings
+} from "lucide-react";
+
+import {Badge} from "@/components/ui/badge";
 import {Logo} from "@/components/logo";
 import {Notification} from "@/app/(admin)/_components/notification";
 import {BottomSidebar} from "@/app/(admin)/_components/bottom-sidebar";
+import {SidebarItem} from "@/app/(admin)/_components/sidebar-item";
 
 const routes = [
     {
         title: "Dashboard",
         route: "/dashboard",
+        icon: Home
     },
     {
         title: "Master data",
         route: "/master",
+        icon: Laptop
     },
     {
         title: "Booking",
         route: "/booking",
+        icon: Package
     },
     {
         title: "Calendar",
         route: "/calendar",
+        icon: Calendar
     },
     {
         title: "Setting",
         route: "/setting",
+        icon: Settings
     }
 ]
 
@@ -50,52 +60,17 @@ export const DesktopSidebar = () => {
                 </div>
                 <div className="flex-1">
                     <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                        <Link
-                            href={"/dashboard"}
-                            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <Home className="h-4 w-4"/>
-                            Dashboard
-                        </Link>
-                        <Link
-                            href={"/data"}
-                            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <Laptop className="h-4 w-4"/>
-                            Master data
-                        </Link>
-                        <Link
-                            href="#"
-                            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <ShoppingCart className="h-4 w-4"/>
-                            Orders
-                            <Badge
-                                className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                                6
-                            </Badge>
-                        </Link>
-                        <Link
-                            href="#"
-                            className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                        >
-                            <Package className="h-4 w-4"/>
-                            Products{" "}
-                        </Link>
-                        <Link
-                            href="#"
-                            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <Users className="h-4 w-4"/>
-                            Customers
-                        </Link>
-                        <Link
-                            href="#"
-                            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <LineChart className="h-4 w-4"/>
-                            Analytics
-                        </Link>
+                        {
+                            routes.map((route, index) => (
+                                <SidebarItem
+                                    title={route.title}
+                                    route={route.route}
+                                    icon={route.icon}
+                                    key={index}
+                                />
+                            ))
+                        }
+
                     </nav>
                 </div>
                 <div className="mt-auto p-4">
--- app/(admin)/_components/sidebar-item.tsx
+++ app/(admin)/_components/sidebar-item.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
-import {Home} from "lucide-react";
+import {LucideIcon} from "lucide-react";
 import React from "react";
 
 interface SidebarItemProps {
     title: string;
     route: string;
+    icon: LucideIcon
 }
 
-export const SidebarItem = ({title, route}: SidebarItemProps) => {
+export const SidebarItem = ({title, route, icon: Icon}: SidebarItemProps) => {
     return(
         <Link
             href={route}
             className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
         >
-            <Home className="h-4 w-4"/>
+            <Icon className="h-4 w-4" />
             {title}
         </Link>
     )
